0a7689a105bbf87491986e3c7cc5f8ebe87e3bb2
Tidiane Dia <atdia97@gmail.com>
2022-06-17 16:52:57 +0100

    Refactor `find_duplicates` to use shortcuts whenever possible
    Update tests as well

--- src/wagtail_images_deduplicator/find_duplicates.py
+++ src/wagtail_images_deduplicator/find_duplicates.py
@@ -1,3 +1,6 @@
+from django.db.models import Q
+from wagtail.images import get_image_model
+
 from .utils import get_max_distance_thresold
 
 
@@ -11,10 +14,37 @@ def find_image_duplicates(image, user, permission_policy):
         user, "choose"
     ).exclude(pk=image.pk)
 
+    file_hash = image.file_hash
+    custom_hash = image.custom_hash
+
+    # Try to shortcut by finding exact duplicates (if any).
+    if custom_hash:
+        if file_hash:
+            duplicates = instances.filter(
+                Q(file_hash=file_hash) | Q(custom_hash=custom_hash)
+            )
+        else:
+            duplicates = instances.filter(custom_hash=custom_hash)
+
+        if duplicates:
+            return duplicates
+
+    elif file_hash:
+        # Custom hash isn't set, resort to finding exact duplicates by file hash.
+        return instances.filter(file_hash=file_hash)
+
+    else:
+        # Neither file hash nor custom hash are set. We can't find duplicates!
+        return get_image_model().objects.none()
+
+    # We haven't found any exact duplicates at this point but the image's custom hash is set.
+    # Let's find the near duplicates (if any).
     image_hash = image.hash_instance
     duplicates = (
         instance.pk
-        for instance in instances.only("pk", "custom_hash").iterator()
+        for instance in instances.exclude(custom_hash="")
+        .only("pk", "custom_hash")
+        .iterator()
         if is_image_duplicate(image_hash, instance.hash_instance)
     )
     return instances.filter(pk__in=duplicates)
--- tests/wagtail_images_deduplicator/test_find_duplicates.py
+++ tests/wagtail_images_deduplicator/test_find_duplicates.py
@@ -7,6 +7,7 @@ from wagtail.images.permissions import permission_policy
 from tests.models import CustomImage
 from tests.utils import get_test_images_files
 from wagtail_images_deduplicator.find_duplicates import find_image_duplicates
+from wagtail_images_deduplicator.models import DuplicateFindingMixin
 
 
 class TestFindDuplicates(TestCase):
@@ -22,11 +23,23 @@ class TestFindDuplicates(TestCase):
         rmtree("original_images")
 
     def find_duplicates(self, image):
-        return (
-            find_image_duplicates(image, self.user, permission_policy)
-            .order_by("pk")
-            .values_list("pk", flat=True)
-        )
+        duplicates = find_image_duplicates(image, self.user, permission_policy)
+        if duplicates._result_cache is None:
+            return duplicates.order_by("pk").values_list("pk", flat=True)
+        return [duplicate.pk for duplicate in duplicates._result_cache]
+
+    def duplicate_image(self, image, copy_file_hash=True, copy_custom_hash=True):
+        """Duplicates an image and saves the copy in the database."""
+
+        image.pk = None
+        image.id = None
+        image._state.adding = True
+        if not copy_file_hash:
+            image.file_hash = ""
+        if not copy_custom_hash:
+            image.custom_hash = ""
+
+        super(DuplicateFindingMixin, image).save()
 
     def test_find_image_duplicates(self):
         duplicates = {
@@ -39,5 +52,81 @@ class TestFindDuplicates(TestCase):
 
         for i, duplicates_expected in duplicates.items():
             with self.subTest(i=i):
-                duplicates_found = self.find_duplicates(self.images[i - 1])
-                self.assertQuerysetEqual(duplicates_found, duplicates_expected)
+                with self.assertNumQueries(3):
+                    self.assertQuerysetEqual(
+                        self.find_duplicates(self.images[i - 1]), duplicates_expected
+                    )
+
+    def test_find_image_duplicates_without_hashes(self):
+        images = CustomImage.objects.all()[:]
+        for i in range(1, 6):
+            with self.subTest(i=i):
+                image = images[i - 1]
+                self.duplicate_image(
+                    image, copy_file_hash=False, copy_custom_hash=False
+                )
+
+                # Image has no file hash nor custom_hash, so no duplicates should be returned.
+                # No queries performed as well.
+                with self.assertNumQueries(0):
+                    self.assertQuerysetEqual(self.find_duplicates(image), [])
+
+    def test_find_image_duplicates_with_only_file_hash(self):
+        images = CustomImage.objects.all()
+        for image in images:
+            # Set file hash
+            with image.open_file() as f:
+                image._set_file_hash(f.read())
+
+            super(DuplicateFindingMixin, image).save(update_fields=["file_hash"])
+
+        for i in range(1, 6):
+            with self.subTest(i=i):
+                image = images[i - 1]
+                self.duplicate_image(image, copy_custom_hash=False)
+
+                # Image has only file hash, only exact duplicates (by file hash) should be returned.
+                with self.assertNumQueries(1):
+                    self.assertQuerysetEqual(self.find_duplicates(image), [i])
+
+    def test_find_image_duplicates_with_only_custom_hash(self):
+        images = CustomImage.objects.all()[:]
+        for i in range(1, 6):
+            with self.subTest(i=i):
+                image = images[i - 1]
+                self.duplicate_image(image)
+
+                # Image has only custom hash, only exact duplicates (by custom hash) should be returned.
+                with self.assertNumQueries(1):
+                    self.assertQuerysetEqual(self.find_duplicates(image), [i])
+
+    def test_find_image_duplicates_with_hashes(self):
+        images = CustomImage.objects.all()
+        for image in images:
+            with image.open_file() as f:
+                image._set_file_hash(f.read())
+
+            super(DuplicateFindingMixin, image).save(update_fields=["file_hash"])
+
+        for i in range(1, 6):
+            with self.subTest(i=i):
+                duplicates = []
+                image = images[i - 1]
+                file_hash = image.file_hash
+
+                # Duplicate the image twice.
+                # One with the file hash removed and the other with the custom hash removed.
+                self.duplicate_image(image, copy_file_hash=False)
+                duplicates.append(image.pk)
+
+                image.file_hash = file_hash
+                self.duplicate_image(image, copy_custom_hash=False)
+                duplicates.append(image.pk)
+
+                original_image = CustomImage.objects.get(pk=i)
+                # The original image has both a custom hash and a file hash,
+                # exact duplicates (by custom hash or file hash) should be returned.
+                # The 2 copies we made above should match as well.
+                with self.assertNumQueries(1):
+                    duplicates_found = self.find_duplicates(original_image)
+                    self.assertQuerysetEqual(duplicates_found, duplicates)
